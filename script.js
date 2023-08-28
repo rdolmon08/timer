2b80c44862672985db4026ac26e24c2863fa4a71
Rebecca D <rdolmon08@gmail.com>
2023-08-28 15:49:19 -0500

    more javascript
    doesn't work yet

--- script.js
+++ script.js
@@ -19,3 +19,41 @@ timerContainer.addEventListener("click", function()
         flag = false;
     }
 })
+
+function startTimer() {
+    if(timeBegan === null)
+    timeBegan = new Date();
+
+    if(timeStopped !== null)
+    stoppedDuration += (new Date() - timeStopped); 
+
+    startInterval = setInterval(clockRunning, 10); 
+
+}
+
+function stopTimer() {
+
+    timeStopped = new Date(); 
+    clearInterval(startInterval); 
+
+}
+
+function clockRunning() {
+
+    var currentTime = new Date(); 
+    var timeElapsed = new Date(currentTime - timeBegan - stoppedDuration); 
+
+    var minutes = timeElapsed.getUTCMinutes(); 
+    var seconds = timeElapsed.getUTCSeconds();
+    var milliseconds = timeElapsed.getUTCMilliseconds(); 
+
+    milliseconds = Math.floor(milliseconds/10); 
+
+    document.getElementById("timer-display").innerHTML = 
+    (minutes = minutes < 10 ? '0' + minutes:minutes) + ":" + 
+    (seconds = seconds < 10 ? '0' + seconds:seconds) + ":" +
+    (milliseconds = milliseconds < 10 ? '0' + milliseconds:milliseconds) + ":" +
+
+}
+
+// https://www.youtube.com/watch?v=_bvutuhUxHY&list=PLtMugc7g4GaqAVDZwQ_t1H6500ZGJzOgW&index=14&ab_channel=WebDevTutorials
